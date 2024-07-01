b84f16d896f6983d598162fa2f0ba1734b91c050
yakuphantonintshi <yakupha58756@gmail.com>
2024-07-01 09:49:54 +0200

    Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="container-fluid">
+      <div class="row">
+          <p class="lead">Yakupha's portfolio &copy;
+          <span class="shadow">
+              {{ cYear }}
+          </span>
+      </p>
+      </div>
+  
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+      name: "FooterComp",
+      data(){
+          return{
+              cYear: new Date().getUTCFullYear()
+          }
+      }, 
+  
+  }
+  </script>
+  
+  <style scoped>
+    .lead{
+        color: #51c4df;
+    }
+  </style>
