75f3c0abcc06662aa7d15e08008c88be0803a8e3
yakuphantonintshi <yakupha58756@gmail.com>
2024-07-01 09:34:11 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <NavBar/>
   </nav>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
 }
+ 
+</script>
 
-nav {
-  padding: 30px;
-}
+<style src="@/assets/css/style.css">
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
